22ad46e3ed12242eef56608414a5079a68aee920
DarkTong <13427508433@163.com>
2017-12-28 21:12:04 +0800

    Update
    Update MainPage

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 import MainPage from '@/components/MainPage'
+import Page2 from '@/components/Page2'
 
 Vue.use(Router)
 
@@ -9,12 +10,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      // name: 'HelloWorld',
+      // component: HelloWorld
+      redirect: '/MainPage'
     }, {
       path: '/MainPage',
       name: 'MainPage',
-      component: MainPage
+      component: MainPage,
+      children: [
+        {
+          name: 'Page2',
+          path: 'Page2',
+          component: Page2
+        }
+      ]
     }
   ]
 })
