7d5aa6fdde79aa5fc792f5fd820174d47d32ddc9
Xiaozh <838223935@qq.com>
2017-12-30 20:46:06 +0800

    登录注册页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 /* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueResource from 'vue-resource'
 // import HelloWorld from '@/components/HelloWorld'
 import MainPage from '@/components/MainPage'
 import PersonalSpace from '@/page/PersonalSpace'
+import Register from '@/page/Register'
+import Login from '@/page/Login'
 import ContentList from '@/components/ContentList'
 import RoadNav from '@/components/RoadNav'
 import ColumnList from '@/components/ColumnList'
 import RecommendList from '@/components/RecommendList'
 
-Vue.use(Router)
+
+Vue.use(Router);
+Vue.use(VueResource);
+
 
 export default new Router({
     routes: [{
@@ -17,7 +23,7 @@ export default new Router({
         // name: 'HelloWorld',
         // component: HelloWorld
         redirect: '/MainPage'
-    }, , {
+    }, {
         path: '/PersonalSpace',
         name: '/PersonalSpace',
         component: PersonalSpace
@@ -35,5 +41,13 @@ export default new Router({
                 recommend_vp: RecommendList,
             }
         }]
+    }, {
+        path: '/Login',
+        name: '/Login',
+        component: Login
+    }, {
+        path: '/Register',
+        name: '/Register',
+        component: Register
     }]
 })
